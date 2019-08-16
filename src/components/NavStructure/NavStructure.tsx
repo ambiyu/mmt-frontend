4ec2de1feb9ebf5d9dcb59ccc009633a114e61e0
Andy <ahua049@aucklanduni.ac.nz>
2019-08-17 02:28:52 +1200

    test

--- src/components/NavStructure/NavStructure.tsx
+++ src/components/NavStructure/NavStructure.tsx
@@ -13,10 +13,11 @@ export default class NavStructure extends React.Component<IProps, {}> {
     public render() {
         return (
             <div className="nav-structure">
+                <div className="box"></div>
                 <Header />
                 <SearchBar handleSearch={this.props.handleSearch} />
                 <Navbar handlePageChange={this.props.handlePageChange} />
-                <div className="box"></div>
+
             </div>
         );
     }
